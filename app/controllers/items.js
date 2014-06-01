2ed9fa1d1dc964c76f6c2999a7e99252bdda2dea
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2014-06-01 16:39:55 +0400

    added live filter

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -6,7 +6,14 @@ var ItemsController = Ember.Controller.extend({
 		unfilter: function(param) {
 			$('html').removeClass('show-'+param);
 		}
-	}
+	},
+	filteredContent : function() {
+	    var searchText = this.get('searchText'),
+	        regex = new RegExp(searchText, 'i');
+	    return this.get('model').filter(function(item){
+	        return regex.test(item.get('text'));
+	    });
+	}.property('searchText', 'model')
 });
 
 export default ItemsController;
